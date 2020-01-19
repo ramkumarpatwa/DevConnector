b281c66a0832226a4f83a8fb37d751215e96cbda
Ram Kumar <ramkumarpatwa123@gmail.com>
2020-01-19 21:53:43 +0530

    routes

---
+++ routes/api/posts.js
@@ -0,0 +1,11 @@
+//add post , like , comment
+
+const express = require('express');
+const router = express.Router();
+
+//@route   GET api/posts
+//@desc    Test route
+//@access  Public
+router.get('/', (req, res) => res.send('Posts route'));
+
+module.exports = router;
